d410a9013662f8f3e811a9ccc0b4b010654391b3
Joshua Ramczyk <JoswaR84@gmail.com>
2019-12-30 10:14:18 -0600

    Update punchout.py

--- punchout.py
+++ punchout.py
@@ -34,48 +34,60 @@ class MainWindow(Frame):
         self.right = 0
         self.bottom = 0
 
-        # Widgets
-        self.config(bg='light grey')
-        self.button_container = Frame(self)        
-        self.button_container.pack(side='top', fill='x', padx=10, pady=(10, 0))        
-        self.pdf_container = Canvas(self, width=850, height=1100)
-        self.pdf_container.pack(pady=(10, 0))
+        # Widget Frames
+        self.config(bg='white')
+        self.button_container = Frame(self)
+        self.button_container.pack(side='top', fill='x', padx=10, pady=(10, 0))
+        self.pdf_container = Canvas(self)
+        self.pdf_container.pack(fill='both', expand=True, padx=10, pady=10)
+        self.pdf_container.bind("<Configure>", self.canvas_resize)
+        # Widget Buttons
         self.open_files_button = Button(self.button_container, text='Select PDFs & Save Location',
-                                        command=self.open_save, font='Helvetica 12 bold', fg='white', bg='dark blue')
+                                        command=self.open_save, bg='lightblue')
         self.open_files_button.pack(fill='x', expand=1)
         self.next_file_button = Button(self.button_container, text='Save Crop & Go To Next', 
-                                       command=self.next_crop_image, font='Helvetica 12 bold', fg='white', bg='dark blue')
+                                       command=self.next_crop_image, bg='lightblue')
         self.next_file_button.pack(fill='x', expand=1)
-        self.display_image_method()
+        # Method Calls
+        self.display_image_method()        
+
+    def canvas_resize(self, event):
+        print(event.width, event.height)
+        self.canvas_width = event.width
+        self.canvas_height = event.height
 
     def open_save(self):
         self.pdf_files = None
         self.image_files = []
         self.current_image = 0
-        self.image_to_crop = None        
-        for old_file in os.listdir(self.temp_path):
-            os.remove(os.path.join(self.temp_path, old_file))
+        self.image_to_crop = None
+        self.delete_temp_images()
         while not self.pdf_files:
             self.pdf_files = askopenfilenames(initialdir='/', filetypes =(('PDF File', '*.pdf'),
-                                             ('All Files','*.*')), title='Choose PDFs to Crop...')        
-        for pdf in self.pdf_files:
-            convert_from_path(pdf, output_folder=self.temp_path, size=(850, 1100))
-        for image in os.listdir(self.temp_path):
-            self.image_files.append(os.path.join(self.temp_path, image))        
-        self.display_image_method()
+                                             ('All Files','*.*')), title='Choose PDFs to Crop...')
         while not self.save_directory:
             self.save_directory = askdirectory(title='Choose Cropped Save Location')
+        for pdf in self.pdf_files:
+            convert_from_path(pdf, output_folder=self.temp_path, dpi=300)
+        for image in os.listdir(self.temp_path):
+            self.image_files.append(os.path.join(self.temp_path, image))
+        self.display_image_method()
 
     def display_image_method(self):
         if not self.image_files or self.current_image >= len(self.image_files):
             self.image_files = []
             self.current_image = 0
             self.image_to_crop = None            
-            self.pdf_image = PhotoImage(file=os.path.join(self.py_module_path, 'placeholder.gif'))
-            self.pdf_container.create_image(0, 0, image=self.pdf_image, anchor=NW)
+            self.welcome_image = PhotoImage(file=os.path.join(self.py_module_path, 'box_boxers.gif'))
+            self.pdf_container.create_image(0, 0, image=self.welcome_image, anchor=NW)
             self.pdf_container.unbind('<Button-1>')
-        else: 
-            self.image_to_display = self.image_files[self.current_image]        
+        else:
+            # working on resizing image based on canvas size
+            # Need to change crop selection box based on scaling
+            # Crop selection box coordinates will be transposed to select unscaled image
+            print(self.canvas_width)
+            print(self.canvas_height)
+            self.image_to_display = self.image_files[self.current_image]
             self.image_to_crop = Image.open(self.image_files[self.current_image])
             self.display_on_canvas = PhotoImage(file=self.image_to_display)
             self.pdf_container.create_image(0, 0, image=self.display_on_canvas, anchor=NW)
@@ -99,11 +111,15 @@ class MainWindow(Frame):
         self.current_image = self.current_image + 1
         self.display_image_method()
 
+    def delete_temp_images(self):
+        for old_file in os.listdir(self.temp_path):
+            os.remove(os.path.join(self.temp_path, old_file))
+
 if __name__ == '__main__':
     root = Tk()    
     main = MainWindow(root)
     main.pack(side='top', fill='both', expand=True)
     root.iconbitmap(os.path.join(main.py_module_path, 'box_gloves.ico'))
-    root.wm_geometry('900x1205')
+    root.wm_geometry('525x475')
     root.title('46 Punch')
     root.mainloop()
